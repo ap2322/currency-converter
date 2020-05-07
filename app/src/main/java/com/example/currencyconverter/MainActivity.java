d90177e4983aed440e5887075e1e73111e69aa24
Alice Post <alicep2322@gmail.com>
2020-05-06 22:08:46 -0600

    Add convertCurrency onClick method to convert from dollars to pounds

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     public void convertCurrency(View view) {
+        EditText dollarsEditText = (EditText) findViewById(R.id.dollarsEditText);
+        Double dollarsDbl = Double.parseDouble(dollarsEditText.getText().toString());
+        Double poundsDbl = dollarsDbl * .81;
+        String poundsStr = String.format("%.2f", poundsDbl);
 
+        Toast.makeText(this, poundsStr, Toast.LENGTH_LONG).show();
     }
 
     @Override
