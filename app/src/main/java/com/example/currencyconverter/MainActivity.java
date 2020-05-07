ba135d1dba1b865443c80dc5966cb34f7cebaf68
Alice Post <alicep2322@gmail.com>
2020-05-06 21:32:11 -0600

    Setup currency converter

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertCurrency(View view) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
